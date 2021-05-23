d8e53ad846110c79be2a78bd19d907f4cb889caf
Chris Barber-Riley <chris.barberriley@gmail.com>
2021-05-23 21:58:07 +0100

    Basic frontend added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { getCryptoPrices } from './features/price/priceSlice';
 
 function App() {
   const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.price);
+
+  useEffect(() => {
+    dispatch(getCryptoPrices());
+  }, [dispatch]);
+
   return (
-    <div>
-      <button type="button" onClick={() => dispatch(getCryptoPrices())}>
-        Fetch data
-      </button>
-    </div>
+    <section className="container">
+      <div className="wave">
+        <div className="panel">
+          {data?.success ? (
+            <>
+              <p>Data retrieved</p>
+              <p>Target: {data.target}</p>
+              {Object.entries(data.rates).map((rate) => (
+                <p>
+                  <strong>{rate[0]}</strong>: {rate[1]}
+                </p>
+              ))}
+            </>
+          ) : (
+            <p>Issue fetching data...</p>
+          )}
+        </div>
+      </div>
+    </section>
   );
 }
 
